b9b724495d0af344cfca30a5db448267f520da13
Naman <naman@DESKTOP-U70D2SO.localdomain>
2025-11-15 17:08:54 +0000

    update

---
+++ Intervals/summary.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class summary {
+
+    public List<String> summaryRanges(int[] nums) {
+        List<String> list = new ArrayList<>();
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        summary obj = new summary();
+        int[] nums = {0,1,2,4,5,7};
+        System.out.println(obj.summaryRanges(nums));
+    }
+}
