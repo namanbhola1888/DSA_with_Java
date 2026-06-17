e834624b9586613164f351d3ab786bf2dd0e6e24
namanbhola1888 <bhola.naman02@gmail.com>
2026-06-17 23:34:06 +0530

    new

---
+++ 191/Arrays/four4Sum.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class four4Sum {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        return list;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {1,0,-1,0,-2,2};
+        int target = 0;
+
+        four4Sum obj = new four4Sum();
+        obj.fourSum(nums, target);
+    }
+}
