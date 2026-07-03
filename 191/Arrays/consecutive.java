40463f259c6a6f84ed00722937daccf672f3176f
namanbhola1888 <bhola.naman02@gmail.com>
2026-07-03 15:26:45 +0530

    updates

---
+++ 191/Arrays/consecutive.java
@@ -0,0 +1,27 @@
+public class consecutive{
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max_count = 0;
+        int count = 0;
+        int n = nums.length;
+
+        for(int num: nums){
+            if(num == 1){
+                count++;
+                max_count = Math.max(max_count, count);
+            }
+            else{
+                count = 0;
+            }
+        }
+
+        return max_count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,0,1,1,1};
+
+        consecutive obj = new consecutive();
+        System.out.println(obj.findMaxConsecutiveOnes(arr));
+    }
+}
---
+++ 191/Arrays/consecutives.java
@@ -0,0 +1,17 @@
+public class consecutive{
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max_count = 1;
+        int count = 0;
+
+        for(int i=0; i<nums.length;)
+        return max_count;
+    }
+
+    public public static void main(String[] args) {
+        int[] arr = {1,1,0,1,1,1};
+
+        consecutive obj = new consecutive();
+        System.out.println(obj.findMaxConsecutiveOnes(arr));
+    }
+}
