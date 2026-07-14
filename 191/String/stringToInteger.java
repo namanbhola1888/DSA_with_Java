bfa04e85ee0764642f2d803e75afce904b46b47d
namanbhola1888 <bhola.naman02@gmail.com>
2026-07-14 15:06:56 +0530

    update

---
+++ 191/String/stringToInteger.java
@@ -0,0 +1,16 @@
+public class stringToInteger{
+
+    public int myAtoi(String s) {
+        
+        
+
+        return 0;   
+    }
+
+    public static void main(String[] args){
+        String s ="-42";
+        stringToInteger obj = new stringToInteger();
+
+        System.err.println(obj.myAtoi(s));
+    }
+}
