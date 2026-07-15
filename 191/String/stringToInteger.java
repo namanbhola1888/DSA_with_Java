1f51d0aac4d6da766d242f611ef5683149326e24
namanbhola1888 <bhola.naman02@gmail.com>
2026-07-15 15:52:01 +0530

    update

--- 191/String/stringToInteger.java
+++ 191/String/stringToInteger.java
@@ -2,9 +2,21 @@ public class stringToInteger{
 
     public int myAtoi(String s) {
         
-        
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
 
-        return 0;   
+        for(int i=0; i<n; i++){
+            char c = s.charAt(i);
+
+            if(c == '-'){
+                sb.append(c);
+            }
+            if(Character.isDigit(c)){
+                sb.append(c);
+            }
+        }
+
+        return Integer.parseInt(sb.toString());   
     }
 
     public static void main(String[] args){
