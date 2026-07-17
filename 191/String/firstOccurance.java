d45ce7ba73ba1ea2829c411eb606619302fc0028
namanbhola1888 <bhola.naman02@gmail.com>
2026-07-17 16:55:07 +0530

    update

--- 191/String/firstOccurance.java
+++ 191/String/firstOccurance.java
@@ -3,15 +3,14 @@ public class firstOccurance {
         int n = haystack.length();
         int m = needle.length();
 
-        if(n < m) return -1;
+        if(m == 0) return -1;
 
-        for(int i=0; i<n-m; i++){
-            for(int j=0; j<m; j++){
-                if(haystack.charAt(i) != needle.charAt(j)) break;
-            }
+        for(int i=0; i<=n-m; i++){
+            String window = haystack.substring(i, i+m);
+            if(window.equals(needle)) return i;
         }
 
-        return 0;
+        return -1;
     }
 
     public static void main(String[] args){
