a58bee1078adedc5e6883abe138f7fce6acbea92
namanbhola1888 <bhola.naman02@gmail.com>
2026-08-29 16:42:10 +0530

    update

--- Logical/sorted_array.py
+++ Logical/sorted_array.py
@@ -1,8 +1,20 @@
 numbers = [2,1,4,1,23]
 
 def check_sorted(numbers):
-    
-    pass
+    n = len(numbers)
+
+    for i in range(n-1):
+        if numbers[i] < numbers[i + 1]:
+            return False
+
+    return True
+
+
+
+def sort_list(numbers):
+    return sorted(numbers)
 
 
 print(check_sorted(numbers))
+
+print(sort_list(numbers))
